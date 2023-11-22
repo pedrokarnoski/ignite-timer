4aabc2eabd985b929bb4af5fa640585493e7f609
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2023-11-21 23:11:57 -0300

    feat: Component status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,25 +20,33 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluída</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluída</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluída</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluída</td>
+              <td>
+                <Status statusColor="red">Interrompida</Status>
+              </td>
             </tr>
           </tbody>
         </table>
